84db2b5318845edce603c4d999e30b5ce1a5c8fe
Thanga Balaji S <thangabalajis@gmail.com>
2018-06-12 23:15:41 +0530

    iterated

--- popup.js
+++ popup.js
@@ -1,12 +1,28 @@
-let changeColor = document.getElementById('changeColor');
-
-changeColor.onclick = function(element) {
-
-    chrome.tabs.getAllInWindow(null,function(tabs){
-        console.log(tabs);
-        for( tab in tabs  ){
-            console.log(tabs[tab]);
+chrome.tabs.getAllInWindow(null,function(tabs){
+    var itemArray = [];
+    for( tab in tabs ){
+        var temp = getDomainFromUrl(tabs[tab].url); 
+        if( itemArray.indexOf( temp ) === -1 ){
+            document.getElementById('popup-space').innerHTML += '<div data-domain="'+temp+'" class="site-list-item">'+temp+'</div>'; 
+            itemArray.push(temp);
         }
-    });
+    }
+});
 
-  };
+var items = document.getElementsByClassName('site-list-item');
+
+console.log(items)
+for(var i = 0; i < items.length; i++) {
+
+    console.log(items[i]);
+    // items[i].addEventListener('click', function(){
+    //     console.log("items[i].getAttribute('data-domain')");
+    // }, false);
+}
+
+function getDomainFromUrl(url){
+
+    return url.split('/')[2] || '';
+    
+
+}

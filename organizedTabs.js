8ca10e789e9ef6f79b00e665728f936c9386feda
Thanga Balaji S <thangabalajis@gmail.com>
2018-06-15 08:46:54 +0530

    local storage added

--- organizedTabs.js
+++ organizedTabs.js
@@ -5,21 +5,67 @@
     var div_target = document.getElementById('target_div');
 
     chrome.tabs.getAllInWindow(null, function (tabs) {
+        var tabsListForLocalStorage = [];
         for (count in tabs) {
-            if (getDomainFromUrl(tabs[count].url) === siteName) {
-                var tab = tabs[count];
-                div_target.innerHTML += '<div class="item-card" > <div class="item-card-image" > <img src="'+ tab.favIconUrl +'"/> </div><div class="item-card-title">'+tab.title +'</div><div class="item-card-link" ><a target="_blank" href="'+ tab.url +'"> Goto Site</a> </div></div>'
+            var tab = tabs[count];
+            var domain = getDomainFromUrl(tab.url);
+            var condition = false;
+            if( siteName === 'all' ){
+                condition = true;
+            }else if (siteName === 'selected'){
+                condition = tab.highlighted;
+                console.log( condition )
+            }else{
+                condition = domain === siteName;
+            }
+            
+            if (domain && condition) {
+
+                var tempTabDetailObject = {
+                    id: tab.id,
+                    title: tab.title,
+                    url: tab.url,
+                    favIcon: tab.favIconUrl,
+                };
+                tabsListForLocalStorage.push(tempTabDetailObject);
                 chrome.tabs.remove(tab.id);
             }
+        }
+        if (localStorage.hasOwnProperty(siteName)) {
+
+            var oldDataOfSite = JSON.parse(localStorage[siteName]);
+            console.log(oldDataOfSite);
+            var newDataOfSite = JSON.stringify(oldDataOfSite.push(tabsListForLocalStorage));
+            localStorage.setItem(siteName,newDataOfSite);
+
+        } else {
+            localStorage.setItem(siteName, JSON.stringify(tabsListForLocalStorage));
         }
 
     });
 
+    renderTabs();
+    function renderTabs() {
+        if (localStorage.hasOwnProperty(siteName)) {
+            var tabsFromSite = JSON.parse(localStorage[siteName]);
+            for (count in tabsFromSite) {
+                var tab = tabsFromSite[count];
+                div_target.innerHTML += '<div class="item-card" > <div class="item-card-image" > <img src="' + tab.favIcon + '"/> </div><div class="item-card-title">' + tab.title + '</div><div class="item-card-link" ><a target="_blank" href="' + tab.url + '"> Goto Site</a> </div></div>'
+            }
+        } else {
+            div_target.innerHTML += '<div>Nothing Found</div>'
+        }
+    }
+
+
     function getDomainFromUrl(url) {
-
-        return url.split('/')[2] || '';
-
-
+        var splitedUrl = url.split('/');
+        if( splitedUrl[0] !== 'chrome-extension:' && splitedUrl[0] !== 'chrome:'  ){
+            return url.split('/')[2] || '';
+        }else{
+           return '';
+        }
+    
     }
 
 

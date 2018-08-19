6e862715d5c8b3d082e60f7a28c1d0a1694897fa
Thanga Balaji S <thangabalajis@gmail.com>
2018-08-19 17:04:19 +0530

    landing page updates

--- docs/js/script.js
+++ docs/js/script.js
@@ -15,3 +15,27 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+
+if( !!window.chrome && !!window.chrome.webstore ){
+
+	document.getElementsByClassName( 'download-btn' )[0].href = "https://chrome.google.com/webstore/detail/infinite-tabs-manager/hjpahkeoabpccidknfdepajnfjncjiep"
+	document.getElementsByClassName( 'download-btn' )[1].href = "https://chrome.google.com/webstore/detail/infinite-tabs-manager/hjpahkeoabpccidknfdepajnfjncjiep"
+
+
+
+}else if ( typeof InstallTrigger !== 'undefined' ){
+
+	document.getElementsByClassName( 'download-btn' )[0].href = "https://addons.mozilla.org/en-US/firefox/addon/infinite-tabs-manager/"
+	document.getElementsByClassName( 'download-btn' )[1].href = "https://addons.mozilla.org/en-US/firefox/addon/infinite-tabs-manager/"
+
+}else{
+
+	document.getElementsByClassName( 'download-btn' )[0].addEventListener('click',function(){
+		alert( "Sorry! This extension is not compatible with your browser yet" );
+		this.target = "";
+	});	document.getElementsByClassName( 'download-btn' )[1].addEventListener('click',function(){
+		alert( "Sorry! This extension is not compatible with your browser yet" );
+		this.target = "";
+	});
+}

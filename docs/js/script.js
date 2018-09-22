8e52560b1676bb768ab27e9537b721b2df13df11
Thanga Balaji S <thangabalajis@gmail.com>
2018-09-22 22:02:43 +0530

    changed download btns

--- docs/js/script.js
+++ docs/js/script.js
@@ -19,22 +19,22 @@ function showDivs(n) {
 
 if( !!window.chrome && !!window.chrome.webstore ){
 
-	document.getElementsByClassName( 'download-btn' )[0].href = "https://chrome.google.com/webstore/detail/infinite-tabs-manager/hjpahkeoabpccidknfdepajnfjncjiep"
-	document.getElementsByClassName( 'download-btn' )[1].href = "https://chrome.google.com/webstore/detail/infinite-tabs-manager/hjpahkeoabpccidknfdepajnfjncjiep"
+	document.getElementsByClassName( 'tryit-btn' )[0].href = "https://chrome.google.com/webstore/detail/infinite-tabs-manager/hjpahkeoabpccidknfdepajnfjncjiep"
+	document.getElementsByClassName( 'tryit-btn' )[1].href = "https://chrome.google.com/webstore/detail/infinite-tabs-manager/hjpahkeoabpccidknfdepajnfjncjiep"
 
 
 
 }else if ( typeof InstallTrigger !== 'undefined' ){
 
-	document.getElementsByClassName( 'download-btn' )[0].href = "https://addons.mozilla.org/en-US/firefox/addon/infinite-tabs-manager/"
-	document.getElementsByClassName( 'download-btn' )[1].href = "https://addons.mozilla.org/en-US/firefox/addon/infinite-tabs-manager/"
+	document.getElementsByClassName( 'tryit-btn' )[0].href = "https://addons.mozilla.org/en-US/firefox/addon/infinite-tabs-manager/"
+	document.getElementsByClassName( 'tryit-btn' )[1].href = "https://addons.mozilla.org/en-US/firefox/addon/infinite-tabs-manager/"
 
 }else{
 
-	document.getElementsByClassName( 'download-btn' )[0].addEventListener('click',function(){
+	document.getElementsByClassName( 'tryit-btn' )[0].addEventListener('click',function(){
 		alert( "Sorry! This extension is not compatible with your browser yet" );
 		this.target = "";
-	});	document.getElementsByClassName( 'download-btn' )[1].addEventListener('click',function(){
+	});	document.getElementsByClassName( 'tryit-btn' )[1].addEventListener('click',function(){
 		alert( "Sorry! This extension is not compatible with your browser yet" );
 		this.target = "";
 	});

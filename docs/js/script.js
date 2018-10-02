993a4da5fb8587f804a41cb7af89066f7df17446
Thanga Balaji S <thangabalajis@gmail.com>
2018-10-02 11:28:28 +0530

    video for docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,22 +1,3 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-
-
 if( !!window.chrome && !!window.chrome.webstore ){
 
 	document.getElementsByClassName( 'download-btn' )[0].href = "https://chrome.google.com/webstore/detail/infinite-tabs-manager/hjpahkeoabpccidknfdepajnfjncjiep"
